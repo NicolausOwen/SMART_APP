5a5e612a96ec516677dba00374c77ae15978a626
NicolausOwen <ww051205@gmail.com>
2025-02-10 16:36:18 +0700

    Added Ziggy.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import './bootstrap';
 import { createInertiaApp, Head, Link } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from './ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
