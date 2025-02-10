b38bd4e1a36b8e0ff32ed99849e285d77b99128c
NicolausOwen <ww051205@gmail.com>
2025-02-10 19:21:10 +0700

    feat(ziggy) Changed import into ziggy.js

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,5 +1,51 @@
-const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":{"sanctum.csrf-cookie":{"uri":"sanctum\/csrf-cookie","methods":["GET","HEAD"]},"sections.index":{"uri":"api\/sections","methods":["GET","HEAD"]},"home":{"uri":"\/","methods":["GET","HEAD"]},"profile.edit":{"uri":"profile","methods":["GET","HEAD"]},"profile.update":{"uri":"profile","methods":["PATCH"]},"profile.destroy":{"uri":"profile","methods":["DELETE"]},"dashboard":{"uri":"dashboard","methods":["GET","HEAD"]},"pengumuman":{"uri":"pengumuman","methods":["GET","HEAD"]},"jadwal":{"uri":"jadwal","methods":["GET","HEAD"]},"cetak":{"uri":"cetak","methods":["GET","HEAD"]},"ubahbio":{"uri":"ubahBio","methods":["GET","HEAD"]},"pendaftaran":{"uri":"pendaftaran","methods":["GET","HEAD"]},"pendaftaran.kefasihan":{"uri":"pendaftaran\/kefasihan-quran","methods":["GET","HEAD"]},"pendaftaran.pendapatan":{"uri":"pendaftaran\/kriteria-pendapatan","methods":["GET","HEAD"]},"pendaftaran.prestasi":{"uri":"pendaftaran\/prestasi","methods":["GET","HEAD"]},"students.index":{"uri":"students","methods":["GET","HEAD"]},"students.create":{"uri":"students\/create","methods":["GET","HEAD"]},"students.store":{"uri":"students","methods":["POST"]},"students.show":{"uri":"students\/{student}","methods":["GET","HEAD"],"parameters":["student"]},"students.edit":{"uri":"students\/{student}\/edit","methods":["GET","HEAD"],"parameters":["student"],"bindings":{"student":"id"}},"students.update":{"uri":"students\/{student}","methods":["PUT","PATCH"],"parameters":["student"],"bindings":{"student":"id"}},"students.destroy":{"uri":"students\/{student}","methods":["DELETE"],"parameters":["student"],"bindings":{"student":"id"}},"admin":{"uri":"admin","methods":["GET","HEAD"]},"panduan.upload":{"uri":"dashboard","methods":["POST"]},"register":{"uri":"register","methods":["GET","HEAD"]},"login":{"uri":"login","methods":["GET","HEAD"]},"password.request":{"uri":"forgot-password","methods":["GET","HEAD"]},"password.email":{"uri":"forgot-password","methods":["POST"]},"password.reset":{"uri":"reset-password\/{token}","methods":["GET","HEAD"],"parameters":["token"]},"password.store":{"uri":"reset-password","methods":["POST"]},"verification.notice":{"uri":"verify-email","methods":["GET","HEAD"]},"verification.verify":{"uri":"verify-email\/{id}\/{hash}","methods":["GET","HEAD"],"parameters":["id","hash"]},"verification.send":{"uri":"email\/verification-notification","methods":["POST"]},"password.confirm":{"uri":"confirm-password","methods":["GET","HEAD"]},"password.update":{"uri":"password","methods":["PUT"]},"logout":{"uri":"logout","methods":["POST"]},"storage.local":{"uri":"storage\/{path}","methods":["GET","HEAD"],"wheres":{"path":".*"},"parameters":["path"]}}};
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+const Ziggy = {
+  "url": "http:\/\/localhost",
+  "port": null,
+  "defaults": {},
+  "routes": {
+    "sanctum.csrf-cookie": { "uri": "sanctum\/csrf-cookie", "methods": ["GET", "HEAD"] },
+    "sections.index": { "uri": "api\/sections", "methods": ["GET", "HEAD"] },
+    "home": { "uri": "\/", "methods": ["GET", "HEAD"] },
+    "profile.edit": { "uri": "profile", "methods": ["GET", "HEAD"] },
+    "profile.update": { "uri": "profile", "methods": ["PATCH"] },
+    "profile.destroy": { "uri": "profile", "methods": ["DELETE"] },
+    "dashboard": { "uri": "dashboard", "methods": ["GET", "HEAD"] },
+    "pengumuman": { "uri": "pengumuman", "methods": ["GET", "HEAD"] },
+    "jadwal": { "uri": "jadwal", "methods": ["GET", "HEAD"] },
+    "cetak": { "uri": "cetak", "methods": ["GET", "HEAD"] },
+    "ubahbio": { "uri": "ubahBio", "methods": ["GET", "HEAD"] },
+    "pendaftaran": { "uri": "pendaftaran", "methods": ["GET", "HEAD"] },
+    "pendaftaran.kefasihan": { "uri": "pendaftaran\/kefasihan-quran", "methods": ["GET", "HEAD"] },
+    "pendaftaran.pendapatan": { "uri": "pendaftaran\/kriteria-pendapatan", "methods": ["GET", "HEAD"] },
+    "pendaftaran.prestasi": { "uri": "pendaftaran\/prestasi", "methods": ["GET", "HEAD"] },
+    "students.index": { "uri": "students", "methods": ["GET", "HEAD"] },
+    "students.create": { "uri": "students\/create", "methods": ["GET", "HEAD"] },
+    "students.store": { "uri": "students", "methods": ["POST"] },
+    "students.show": { "uri": "students\/{student}", "methods": ["GET", "HEAD"], "parameters": ["student"] },
+    "students.edit": { "uri": "students\/{student}\/edit", "methods": ["GET", "HEAD"], "parameters": ["student"], "bindings": { "student": "id" } },
+    "students.update": { "uri": "students\/{student}", "methods": ["PUT", "PATCH"], "parameters": ["student"], "bindings": { "student": "id" } },
+    "students.destroy": { "uri": "students\/{student}", "methods": ["DELETE"], "parameters": ["student"], "bindings": { "student": "id" } },
+    "admin": { "uri": "admin", "methods": ["GET", "HEAD"] },
+    "panduan.upload": { "uri": "dashboard", "methods": ["POST"] },
+    "register": { "uri": "register", "methods": ["GET", "HEAD"] },
+    "login": { "uri": "login", "methods": ["GET", "HEAD"] },
+    "password.request": { "uri": "forgot-password", "methods": ["GET", "HEAD"] },
+    "password.email": { "uri": "forgot-password", "methods": ["POST"] },
+    "password.reset": { "uri": "reset-password\/{token}", "methods": ["GET", "HEAD"], "parameters": ["token"] },
+    "password.store": { "uri": "reset-password", "methods": ["POST"] },
+    "verification.notice": { "uri": "verify-email", "methods": ["GET", "HEAD"] },
+    "verification.verify": { "uri": "verify-email\/{id}\/{hash}", "methods": ["GET", "HEAD"], "parameters": ["id", "hash"] },
+    "verification.send": { "uri": "email\/verification-notification", "methods": ["POST"] },
+    "password.confirm": { "uri": "confirm-password", "methods": ["GET", "HEAD"] },
+    "password.update": { "uri": "password", "methods": ["PUT"] },
+    "logout": { "uri": "logout", "methods": ["POST"] },
+    "storage.local": { "uri": "storage\/{path}", "methods": ["GET", "HEAD"], "wheres": { "path": ".*" }, "parameters": ["path"] }
+  }
+};
+
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
   Object.assign(Ziggy.routes, window.Ziggy.routes);
 }
-export { Ziggy };
+
+export { Ziggy, ZiggyVue };
